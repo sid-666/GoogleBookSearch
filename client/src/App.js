f397773ab42a3c1bbcd4aa25e9463a6539f74898
Seance <siddharth.dhananjay@gmail.com>
2021-01-19 13:35:22 +1100

    did the paths for and route  in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import Books from "./pages/Books";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer"
+import "./App.css"
 
 function App() {
   return (
+  <Router>
     <div>
       <Nav />
-      <Books />
+      <Switch>
+        <Route exact path="/" component={SearchBooks} />
+        <Route exact path="/saved" component={SaveBooks} />
+        <Route exact path="/saved/:id" component={SaveBooks} />
+        <Route component={NoMatch} /> 
+      </Switch>
+      <Footer />
     </div>
-  );
+  </Router>
+);
 }
 
 export default App;
